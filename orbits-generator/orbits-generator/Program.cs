7ac1ac0f178dae17008bf4ae6c594d475bdf10f1
tfedoris <fedoristyler@gmail.com>
2017-09-10 10:34:29 -0400

    Empty project setup

---
+++ orbits-generator/orbits-generator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orbits_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
